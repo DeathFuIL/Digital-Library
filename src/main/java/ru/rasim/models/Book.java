58f4cc652aad868b7bdc40ac0f47d8f84f459b88
Rasim <rasimulka48@mail.ru>
2023-08-17 16:08:48 +0300

    add Book model

---
+++ src/main/java/ru/rasim/models/Book.java
@@ -0,0 +1,28 @@
+package ru.rasim.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    private Integer id;
+
+    @NotEmpty(message = "Name must not be empty")
+    private String name;
+
+    @Min(value = 0, message = "Age must be greater than 0")
+    private Integer year;
+
+    @NotEmpty(message = "Author must not be empty")
+    private String author;
+
+}
