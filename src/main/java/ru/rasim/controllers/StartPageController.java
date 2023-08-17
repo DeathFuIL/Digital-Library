6f88b4c483fadbe85dd4bd6e9a9113819e77cb72
Rasim <rasimulka48@mail.ru>
2023-08-17 16:09:40 +0300

    add controller for start page

---
+++ src/main/java/ru/rasim/controllers/StartPageController.java
@@ -0,0 +1,12 @@
+package ru.rasim.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartPageController {
+    @GetMapping()
+    public String startPage() {
+        return "index.html";
+    }
+}
