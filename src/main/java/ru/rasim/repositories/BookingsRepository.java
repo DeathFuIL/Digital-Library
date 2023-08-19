ca32ceaf8dc0eda9e8724fd1f09152e7c432f17c
Rasim <rasimulka48@mail.ru>
2023-08-19 18:32:48 +0300

    add new methods

--- src/main/java/ru/rasim/repositories/BookingsRepository.java
+++ src/main/java/ru/rasim/repositories/BookingsRepository.java
@@ -2,5 +2,14 @@ package ru.rasim.repositories;
 
 import ru.rasim.models.Booking;
 
+import java.util.List;
+
 public interface BookingsRepository extends CrudRepository<Booking> {
+    boolean finish(Integer id);
+
+    List<Booking> showAllActive();
+
+    List<Booking> showAllFinished();
+
+    List<Booking> showByPersonId(Integer id);
 }
